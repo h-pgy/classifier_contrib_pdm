928687f2f86c7dedf5c1a30838fdc37607d147d3
h-pgy <hpougy@gmail.com>
2025-02-12 23:40:05 -0300

    bag of words dataframe

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -18,8 +18,29 @@ def save_df(df:pd.DataFrame, fname:str)->str:
 
     return fname
 
-def read_df(fname:str)->pd.DataFrame:
+def read_df(fname:str, **read_kwargs)->pd.DataFrame:
 
     fpath: str = os.path.join(DATA_DIR, fname)
 
-    return pd.read_csv(fpath, sep=';')
+    df: pd.DataFrame = pd.read_csv(fpath, sep=';', **read_kwargs)
+
+    if 'Unnamed: 0' in df.columns:
+        df.drop('Unnamed: 0', axis=1, inplace=True)
+    
+    return df
+
+def save_df_parquet(df:pd.DataFrame, fname:str)->str:
+
+    if not fname.endswith('.parquet'):
+        fname = fname+'.parquet'
+    fpath: str = os.path.join(DATA_DIR, fname)
+
+    df.to_parquet(fpath)
+
+    return fpath
+
+def read_df_parquet(fname:str)->pd.DataFrame:
+
+    fpath: str = os.path.join(DATA_DIR, fname)
+
+    return pd.read_parquet(fpath)
