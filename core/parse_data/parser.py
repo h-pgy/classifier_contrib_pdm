ec7c1473996667750f0abc5ed6a60f8a3bc39b8b
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2025-03-05 20:37:59 -0300

    Finetuning svc (#4)
    * simplifying reqs
    
    * suport vector classifier fine tunned - nice results
    
    * f1 status 0.72

--- core/parse_data/parser.py
+++ core/parse_data/parser.py
@@ -28,10 +28,19 @@ class Parser:
 
         return df
     
+    def remove_duplicates_excert(self, df:pd.DataFrame)->pd.DataFrame:
+
+        #em alguns casos os mesmos excertos foram utilizados para duas categorias
+
+        df = df[~df['excerto'].duplicated(keep='first')].reset_index(drop=True)
+
+        return df
+    
     def __call__(self, data:dict[str, JSON_LIST])->pd.DataFrame:
 
         df = self.parse_data(data)
         df = self.remove_duplicates(df)
+        df = self.remove_duplicates_excert(df)
 
         return df
 
